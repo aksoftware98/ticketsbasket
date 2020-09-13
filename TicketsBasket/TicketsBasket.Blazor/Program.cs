f4ead4a342055fe5f2ce99a73772ed63208931dd
aksoftware98 <aksoftware98@hotmail.com>
2020-09-13 20:27:35 +0300

    Creating the Blazor WebAssembly Client Application Configured with Azure Active Directory B2C Authentication

---
+++ TicketsBasket/TicketsBasket.Blazor/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace TicketsBasket
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://ticketsbasket.onmicrosoft.com/44db06c5-8332-480b-9f7d-70cefe94d8ae/Api.ReadWrite");
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
